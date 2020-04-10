62da208cb791ce978b39193ff93816b535c0c0c6
ochiyu <yuya-ochiiai@outlook.jp>
2020-04-11 00:15:08 +0900

    [remove] HelloWorld.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to OCHIYU PAGE"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
